95e801fef9baa58bcdfea55cd22d55339a2fdd33
niksacdev <nikhil.sachdeva@gmail.com>
2018-02-01 16:58:30 +0800

    added vehicle data entity added serialization support for all entities

--- samples.microservice.core/Entity.cs
+++ samples.microservice.core/Entity.cs
@@ -1,9 +1,29 @@
-﻿namespace samples.microservice.core
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace samples.microservice.core
 {
     public abstract class Entity
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
         public string Description { get; set; }
+
+        [JsonProperty(PropertyName = "custom")]
+        public Dictionary<string, string> CustomProperties { get; set; }
+
+        /// <summary>
+        /// serialize JSON into string
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
     }
 }
---
+++ samples.microservice.entities/VehicleData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using Newtonsoft.Json;
+using samples.microservice.core;
+
+namespace samples.microservice.entities
+{
+    public class VehicleData: Entity
+    {
+        [JsonProperty(PropertyName = "vin")]
+        public string Vin { get; set; }
+
+        [JsonProperty(PropertyName = "esn")]
+        public string Esn { get; set; }
+
+        [JsonProperty(PropertyName = "fuel")]
+        public int FuelGauge { get; set; }
+
+        [JsonProperty(PropertyName = "pressure")]
+        public int PressureGauge { get; set; }
+
+        [JsonProperty(PropertyName = "indicatorlight")]
+        public bool IndicatorLights { get; set; }
+
+        [JsonProperty(PropertyName = "headlight")]
+        public bool HeadLights { get; set; }
+
+    }
+}
