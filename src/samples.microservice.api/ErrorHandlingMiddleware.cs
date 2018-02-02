2061d0a9cd5f79eac1af7980464987acb67bba02
niksacdev <nikhil.sachdeva@gmail.com>
2018-02-02 13:17:24 +0800

    added docker support for running api fixed project references to point to package repos instead of local nuget

--- src/samples.microservice.api/ErrorHandlingMiddleware.cs
+++ src/samples.microservice.api/ErrorHandlingMiddleware.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 
-namespace samples.microservice.core
+namespace samples.microservice.api
 {
     public class ErrorHandlingMiddleware
     {
